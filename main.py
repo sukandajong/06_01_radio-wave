26561659b10a5ddfd20dd60bb9c3d0c791a54ae1
sukandajong <sukandajong@gmail.com>
2026-05-04 06:15:54 +0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,43 +1,21 @@
+# iBIT.set_motor(ibitMotorCH.M1, ibitMotor.BACKWARD, 0)
+# iBIT.set_motor(ibitMotorCH.M2, ibitMotor.FORWARD, 0)
+
 def on_received_string(receivedString):
-    if receivedString == "F":
-        basic.show_leds("""
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            """)
-
+    if receivedString == "C":
+        basic.show_icon(IconNames.HEART)
+    elif receivedString == "W":
+        basic.show_icon(IconNames.NO)
+    elif receivedString == "F":
+        basic.show_arrow(ArrowNames.NORTH)
     elif receivedString == "B":
-        basic.show_leds("""
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            """)
-
+        basic.show_arrow(ArrowNames.SOUTH)
     elif receivedString == "R":
-        basic.show_leds("""
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            """)
-
+        basic.show_arrow(ArrowNames.EAST)
     elif receivedString == "L":
-        basic.show_leds("""
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            """)
-
-    else:
-        basic.show_icon(IconNames.SQUARE)
-
+        basic.show_arrow(ArrowNames.WEST)
+    elif receivedString == "S":
+        basic.show_icon(IconNames.SMALL_SQUARE)
 radio.on_received_string(on_received_string)
 
 joy_y = 0
@@ -52,104 +30,27 @@ def on_forever():
     global is_active, joy_x, joy_y
     if pins.digital_read_pin(DigitalPin.P13) == 0:
         is_active = True
-        basic.show_string("ON")
         basic.show_icon(IconNames.HEART)
+        radio.send_string("C")
     elif pins.digital_read_pin(DigitalPin.P16) == 0:
-        is_active = False
-        basic.show_string("OFF")
+        basic.show_icon(IconNames.NO)
+        radio.send_string("W")
     if is_active:
         joy_x = pins.analog_read_pin(AnalogReadWritePin.P2)
         joy_y = pins.analog_read_pin(AnalogReadWritePin.P1)
         if joy_y < 400:
             radio.send_string("F")
-            basic.show_leds("""
-                . . # . .
-                . # # # .
-                # . # . #
-                . . # . .
-                . . # . .
-                """)
+            basic.show_arrow(ArrowNames.NORTH)
         elif joy_y > 600:
             radio.send_string("B")
-            basic.show_leds("""
-                . . # . .
-                . . # . .
-                # . # . #
-                . # # # .
-                . . # . .
-                """)
+            basic.show_arrow(ArrowNames.SOUTH)
         elif joy_x < 400:
             radio.send_string("R")
-            basic.show_leds("""
-                . . # . .
-                . . . # .
-                # # # # #
-                . . . # .
-                . . # . .
-                """)
+            basic.show_arrow(ArrowNames.EAST)
         elif joy_x > 600:
             radio.send_string("L")
-            basic.show_leds("""
-                . . # . .
-                . # . . .
-                # # # # #
-                . # . . .
-                . . # . .
-                """)
+            basic.show_arrow(ArrowNames.WEST)
         else:
             radio.send_string("S")
-            basic.show_icon(IconNames.HEART)
+            basic.show_icon(IconNames.SMALL_SQUARE)
 basic.forever(on_forever)
-
-def on_forever2():
-    global is_active, joy_x, joy_y
-    if pins.digital_read_pin(DigitalPin.P14) == 0:
-        is_active = True
-        basic.show_string("ON")
-        basic.show_icon(IconNames.HEART)
-    elif pins.digital_read_pin(DigitalPin.P16) == 0:
-        is_active = False
-        basic.show_string("OFF")
-    if is_active:
-        joy_x = pins.analog_read_pin(AnalogReadWritePin.P2)
-        joy_y = pins.analog_read_pin(AnalogReadWritePin.P1)
-        if joy_y < 400:
-            radio.send_string("F")
-            basic.show_leds("""
-                . . # . .
-                . # # # .
-                # . # . #
-                . . # . .
-                . . # . .
-                """)
-        elif joy_y > 600:
-            radio.send_string("B")
-            basic.show_leds("""
-                . . # . .
-                . . # . .
-                # . # . #
-                . # # # .
-                . . # . .
-                """)
-        elif joy_x < 400:
-            radio.send_string("R")
-            basic.show_leds("""
-                . . # . .
-                . . . # .
-                # # # # #
-                . . . # .
-                . . # . .
-                """)
-        elif joy_x > 600:
-            radio.send_string("L")
-            basic.show_leds("""
-                . . # . .
-                . # . . .
-                # # # # #
-                . # . . .
-                . . # . .
-                """)
-        else:
-            radio.send_string("S")
-            basic.show_icon(IconNames.HEART)
-basic.forever(on_forever2)
